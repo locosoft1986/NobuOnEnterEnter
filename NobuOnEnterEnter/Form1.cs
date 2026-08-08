0498a1c28a7249bcf7984ef21104f747211e790c
JeremyWeasley <locosoft1986@gmail.com>
2026-08-08 09:53:49 +0800

    try to fix sub window stuck on victory

--- NobuOnEnterEnter/Form1.cs
+++ NobuOnEnterEnter/Form1.cs
@@ -523,6 +523,7 @@ namespace NobuOnEnterEnter
                     windowInfo.KeySequence.Add(new KeyAction(VK_RETURN, battleWaitTimeMs, 50));
                     windowInfo.KeySequence.Add(new KeyAction(VK_RETURN, 1000, 50, true));
                     windowInfo.KeySequence.Add(new KeyAction(VK_RETURN, 1000, 50, true));
+                    windowInfo.KeySequence.Add(new KeyAction(VK_RETURN, 1000, 100, true));
                     windowInfo.KeySequence.Add(new KeyAction(VK_RETURN, 1000, 50, true));
                     windowInfo.KeySequence.Add(new KeyAction(VK_ESCAPE, 1000, 50, true));
                     windowInfo.KeySequence.Add(new KeyAction(VK_ESCAPE, 1000, 50, true));
@@ -552,6 +553,7 @@ namespace NobuOnEnterEnter
                     windowInfo.KeySequence.Add(new KeyAction(VK_RETURN, 1000, 50, true));
                     windowInfo.KeySequence.Add(new KeyAction(VK_RETURN, 1000, 50, true));
                     windowInfo.KeySequence.Add(new KeyAction(VK_RETURN, 1000, 50, true));
+                    windowInfo.KeySequence.Add(new KeyAction(VK_RETURN, 1000, 100, true));
                     windowInfo.KeySequence.Add(new KeyAction(VK_ESCAPE, 1000, 50, true));
                     windowInfo.KeySequence.Add(new KeyAction(VK_ESCAPE, 1000, 50, true));
                     windowInfo.KeySequence.Add(new KeyAction(VK_ESCAPE, 1000, 50, true));
